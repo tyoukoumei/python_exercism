0642adc94bf51cdffa6f4dccbeb38b1c04eee864
tyoukoumei <38462183+tyoukoumei@users.noreply.github.com>
2018-06-12 21:41:48 +0900

    Update atbash_cipher.py

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,6 +1,21 @@
-def encode(plain_text):
-    pass
+from string import ascii_lowercase
+Length = 5
+Plain = ascii_lowercase
+Cipher = ascii_lowercase[::-1]
 
+def base_trans(text):
+    Emp = ''
+    for i in text:
+        if i.isalnum():
+            Emp += i
+    return Emp.lower().translate(Emp.maketrans(Plain,Cipher))
 
-def decode(ciphered_text):
-    pass
+def encode(plain):
+    cipher1 = base_trans(plain)
+    cipher1_list = []
+    for i in range(0, len(cipher1), Length):
+        cipher1_list.append((cipher1[i:i + Length]))
+    return " ".join(cipher1_list)
+
+def decode(ciphered):
+    return base_trans(ciphered)
