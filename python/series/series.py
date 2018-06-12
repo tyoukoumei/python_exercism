bf1ec4525fe717308e492ba74815f2c78b85a46c
tyoukoumei <38462183+tyoukoumei@users.noreply.github.com>
2018-06-12 21:13:48 +0900

    Update series.py

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,8 @@
 def slices(series, length):
-    pass
+    slice_list = []
+    if length <= 0 or length > len(series):
+        raise ValueError("The length does not meet the requirements")
+    else:
+        for i in range(len(series) - length + 1):
+            slice_list.append([int(j) for j in series[i:i+length]])
+    return slice_list
