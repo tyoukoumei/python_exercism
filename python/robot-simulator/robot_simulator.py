06f1d61c2455c3ba1ed634978dfeaff4d6e46b17
tyoukoumei <38462183+tyoukoumei@users.noreply.github.com>
2018-06-12 21:38:54 +0900

    Update robot_simulator.py

--- python/robot-simulator/robot_simulator.py
+++ python/robot-simulator/robot_simulator.py
@@ -1,11 +1,38 @@
 # Globals for the bearings
 # Change the values as you see fit
-EAST = None
-NORTH = None
-WEST = None
-SOUTH = None
+EAST = 0
+NORTH = 1
+WEST = 2
+SOUTH = 3
 
 
-class Robot(object):
+class Robot:
     def __init__(self, bearing=NORTH, x=0, y=0):
-        pass
+        self.bearing = bearing
+        self.coordinates = (x, y)
+        
+    def turn_right(self):
+        self.bearing = (self.bearing - 1) % 4
+        
+    def turn_left(self):
+        self.bearing = (self.bearing + 1) % 4
+        
+    def advance(self):
+        x, y = self.coordinates
+        if self.bearing == NORTH:
+            self.coordinates = (x, y+1)
+        elif self.bearing == SOUTH:
+            self.coordinates = (x, y-1)
+        elif self.bearing == EAST:
+            self.coordinates = (x+1, y)
+        elif self.bearing == WEST:
+            self.coordinates = (x-1, y)
+            
+    def simulate(self, path):
+        for i in path:
+            if i == 'A':
+                self.advance()
+            if i == 'L':
+                self.turn_left()
+            if i == 'R':
+                self.turn_right()
