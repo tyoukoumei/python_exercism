bdc91eab97e073e41b7270cbc8d477da5af58a1b
tyoukoumei <38462183+tyoukoumei@users.noreply.github.com>
2018-06-12 21:40:31 +0900

    Update sieve.py

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,2 +1,17 @@
 def sieve(limit):
-    pass
+    limit_list = []
+    if limit == 1:
+        return limit_list
+    elif limit == 2:
+        limit_list.append(2)
+        return limit_list
+    else:
+        for i in range(2,limit+1):
+            a = 0
+            for j in range(2,i-1):
+                if i % j == 0:
+                    a = 1
+                    break
+            if a == 0:
+                limit_list.append(i)
+        return limit_list
