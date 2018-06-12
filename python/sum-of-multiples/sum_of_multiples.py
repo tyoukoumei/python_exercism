f6887a3e1bd4813c7afc52880b3aaeae7de0bf1e
tyoukoumei <38462183+tyoukoumei@users.noreply.github.com>
2018-06-12 22:03:09 +0900

    Update sum_of_multiples.py

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,8 @@
 def sum_of_multiples(limit, multiples):
-    pass
+    sum_list = []
+    for i in range(limit):
+        for j in multiples:
+            if i%j == 0:
+                sum_list.append(i)
+                break
+    return sum(sum_list)
